081c73d6583854937ba3b94bad7ae51a79bf583d
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-10-02 23:52:26 -0300

    Fazendo get da API de categorias

--- lib/Screens/category/category_repository.dart
+++ lib/Screens/category/category_repository.dart
@@ -1,16 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:poo_project/Screens/pageView/model/modelo_categoria.dart';
 import '../../core/constants/str_constant.dart';
 
-/*
 class CategoryRepository {
-  Future <List<>> getServices() async {
-    var response = await http.get(Uri.parse('$kBaseUrl/services'));
+  Future<List<ModeloCategoria>> getServices() async {
+    var response =
+        await http.get(Uri.parse('$kBaseUrl/servicos_has_categoria'));
     print(response.statusCode);
     List listServices = json.decode(response.body);
-    return listServices.map((json) => )
+    return listServices.map((json) => ModeloCategoria.fromJson(json)).toList();
   }
 }
-*/
--- lib/Screens/category/category_screen.dart
+++ lib/Screens/category/category_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:poo_project/Screens/pageView/model/modelo_categoria.dart';
+
+import 'category_repository.dart';
 
 class CategoryScreen extends StatefulWidget {
-  // /servicos_has_categoria.
   const CategoryScreen({super.key});
 
   @override
@@ -11,11 +13,39 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  CategoryRepository controller = CategoryRepository();
+  late Future<List<ModeloCategoria>> getServices;
+
+  @override
+  void initState() {
+    super.initState();
+    getServices = controller.getServices();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(children: []),
+      body: FutureBuilder<List<ModeloCategoria>>(
+        future: getServices,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: ((context, index) {
+                ModeloCategoria model = snapshot.data![index];
+                return ListTile(
+                  title: Text(model.nome),
+                );
+              }),
+            );
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
