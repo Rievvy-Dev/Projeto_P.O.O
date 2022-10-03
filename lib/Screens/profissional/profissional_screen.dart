685386379853179a047b08564d874e7b7b91195e
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-10-03 03:12:15 -0300

    Implementando o OpenWhatsapp

--- lib/Screens/profissional/profissional_screen.dart
+++ lib/Screens/profissional/profissional_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:poo_project/Screens/pageView/model/modelo_usuario.dart';
 import 'package:poo_project/core/constants/app_colors.dart';
+import 'package:open_whatsapp/open_whatsapp.dart';
 
 class ProfissionalScreen extends StatefulWidget {
   const ProfissionalScreen({super.key});
@@ -11,6 +13,8 @@ class ProfissionalScreen extends StatefulWidget {
 }
 
 class _ProfissionalScreenState extends State<ProfissionalScreen> {
+  ModeloUsuario? userModel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,12 +33,15 @@ class _ProfissionalScreenState extends State<ProfissionalScreen> {
                   Icons.person,
                   size: 40,
                 ),
-                title: Text('Name?'),
+                title: Text('Nome?'),
                 subtitle: Text('Cidade? - Bairro?'),
               ),
               const Spacer(),
               ElevatedButton.icon(
-                onPressed: () {},
+                onPressed: () {
+                  FlutterOpenWhatsapp.sendSingleMessage(
+                      '+5587999013100', 'Mensagem do Flutter');
+                },
                 icon: const Icon(Icons.chat_bubble_outline_rounded),
                 label: const Text('Conversar'),
                 style: ButtonStyle(
