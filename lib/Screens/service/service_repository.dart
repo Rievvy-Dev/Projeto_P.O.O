cbec4cd3cc64f83f8b2993267c8ccb057c96ecac
Rievvy <thiagoalmeidadepaiva@gmail.com>
2022-10-03 10:59:05 -0300

    get da ModeloPrestadorServicos

---
+++ lib/Screens/service/service_repository.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:poo_project/Screens/pageView/model/modelo_prestador_servicos.dart';
+import 'package:poo_project/core/constants/str_constant.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceRepository {
+  Future<List<ModeloPrestadorServicos>> getPrestadores() async {
+    var response = await http.get(Uri.parse(
+        '$kBaseUrl/prestadores-servicos/ver-todos-prestadores-servicos-na-cidade-cidade-exemplo'));
+    print(response.statusCode);
+    List listServices = json.decode(response.body);
+    return listServices
+        .map((json) => ModeloPrestadorServicos.fromJson(json))
+        .toList();
+  }
+}
--- lib/Screens/service/service_screen.dart
+++ lib/Screens/service/service_screen.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:poo_project/Screens/pageView/model/modelo_categoria.dart';
+import 'package:poo_project/Screens/pageView/model/modelo_prestador_servicos.dart';
+import 'package:poo_project/Screens/pageView/model/modelo_servicos_has_categorias.dart';
+import 'package:poo_project/Screens/service/service_repository.dart';
 
-class ServiceScreen extends StatefulWidget {
-  const ServiceScreen({super.key});
+class CategoryScreen extends StatefulWidget {
+  const CategoryScreen({super.key});
 
   @override
-  State<ServiceScreen> createState() => _ServiceScreenState();
+  State<CategoryScreen> createState() => _CategoryScreenState();
 }
 
-class _ServiceScreenState extends State<ServiceScreen> {
+class _CategoryScreenState extends State<CategoryScreen> {
+  ServiceRepository controller = ServiceRepository();
+  late Future<List<ModeloPrestadorServicos>> getPrestadores;
+
+  @override
+  void initState() {
+    super.initState();
+    getPrestadores = controller.getPrestadores();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
+      body: FutureBuilder<List<ModeloPrestadorServicos>>(
+        future: getPrestadores,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: ((context, index) {
+                ModeloPrestadorServicos model = snapshot.data![index];
+                return ListTile(
+                  title: Text(model.nome.toString()),
+                );
+              }),
+            );
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
